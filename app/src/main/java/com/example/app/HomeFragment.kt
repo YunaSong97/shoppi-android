8f59c01ff824e6e60d3ed75b8ce3f2313a58ae53
YunaSong97 <unme97@naver.com>
2022-05-12 21:26:09 +0900

    HomeFragment

--- app/src/main/java/com/example/app/HomeFragment.kt
+++ app/src/main/java/com/example/app/HomeFragment.kt
@@ -1,14 +1,16 @@
 package com.example.app
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import org.json.JSONObject
 
-class HomeFragment: Fragment() {
+class HomeFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,6 +26,27 @@ class HomeFragment: Fragment() {
         val button = view.findViewById<Button>(R.id.btn_enter_product_detail)
         button.setOnClickListener {
             findNavController().navigate(R.id.action_home_to_product_detail)
+        }
+
+        val assetLoader = AssetLoader()
+        val homeData = assetLoader.getJsonString(requireContext(), "home.json")
+        Log.d("homeData", homeData ?: "")
+
+        if (!homeData.isNullOrEmpty()) {
+            val jsonObject = JSONObject(homeData)
+            val title = jsonObject.getJSONObject("title")
+            val text = title.getString("text")
+            val iconUrl = title.getString("icon_url")
+            val titleValue = Title(text, iconUrl)
+            titleValue.text
+            /*val topBanners = jsonObject.getJSONArray("top_banners")
+            val firstBanner = topBanners.getJSONObject(0)
+            val label = firstBanner.getString("label")
+            val productDetail = firstBanner.getJSONObject("product_detail")
+            val price = productDetail.getInt("price")
+
+            Log.d("title", "text=${text}, iconUrl=${iconUrl}")
+            Log.d("firstBanner", "label=${label}, price=${price}")*/
         }
     }
 }
