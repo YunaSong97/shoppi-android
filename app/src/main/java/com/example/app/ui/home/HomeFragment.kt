6aa1de1f50461a424b3bc406f245a5b83aea3fc4
YunaSong97 <unme97@naver.com>
2022-05-14 16:05:26 +0900

    Data binding

--- app/src/main/java/com/example/app/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/app/ui/home/HomeFragment.kt
@@ -9,55 +9,61 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.viewpager2.widget.ViewPager2
-import com.example.app.*
+import com.example.app.GlideApp
+import com.example.app.R
+import com.example.app.databinding.FragmentHomeBinding
 import com.example.app.ui.common.ViewModelFactory
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
 class HomeFragment : Fragment() {
 
-    private val viewModel: HomeViewModel by viewModels{ ViewModelFactory(requireContext()) }
+    private val viewModel: HomeViewModel by viewModels { ViewModelFactory(requireContext()) }
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        var toolbarTitle = view.findViewById<TextView>(R.id.toolbar_home_title)
-        var toolbarIcon = view.findViewById<ImageView>(R.id.toolbar_home_icon)
-        val viewpager = view.findViewById<ViewPager2>(R.id.viewpager_home_banner)
-        val viewPagerIndicator = view.findViewById<TabLayout>(R.id.viewpager_home_banner_indicator)
+        binding.lifecycleOwner = viewLifecycleOwner
+        setToolbar()
+        setTopBanners()
+    }
 
+    private fun setToolbar() {
         viewModel.title.observe(viewLifecycleOwner) { title ->
-            toolbarTitle.text = title.text
-            GlideApp.with(this)
-                .load(title.iconUrl)
-                .into(toolbarIcon)
+            binding.title = title
         }
-        viewpager.adapter = HomeBannerAdapter().apply {
-            viewModel.topBanners.observe(viewLifecycleOwner) { banners ->
+    }
+
+    private fun setTopBanners() {
+        with(binding.viewpagerHomeBanner) {
+            adapter = HomeBannerAdapter().apply {
+                viewModel.topBanners.observe(viewLifecycleOwner) { banners ->
                     submitList(banners)
+                }
             }
+
+            val pageWidth = resources.getDimension(R.dimen.viewpager_item_widwth)
+            val pageMargin = resources.getDimension(R.dimen.viewpager_item_margin)
+            val screenWidth = resources.displayMetrics.widthPixels
+            val offset = screenWidth - pageWidth - pageMargin
+
+            offscreenPageLimit = 3
+            setPageTransformer { page, position ->
+                page.translationX = position * -offset
+            }
+            TabLayoutMediator(binding.viewpagerHomeBannerIndicator, this) { tab, position ->
+
+            }.attach()
         }
-
-        val pageWidth = resources.getDimension(R.dimen.viewpager_item_widwth)
-        val pageMargin = resources.getDimension(R.dimen.viewpager_item_margin)
-        val screenWidth = resources.displayMetrics.widthPixels
-        val offset = screenWidth - pageWidth - pageMargin
-
-        viewpager.offscreenPageLimit = 3
-        viewpager.setPageTransformer { page, position ->
-            page.translationX = position * -offset
-        }
-        TabLayoutMediator(viewPagerIndicator, viewpager) { tab, position ->
-
-        }.attach()
-
     }
 }
